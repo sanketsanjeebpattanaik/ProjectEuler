1536932ff93110e261bb509da583de38e82cab5e
sanket06 <53606569+sanket06@users.noreply.github.com>
2019-08-10 14:58:10 +0530

    Create Prob5.java

---
+++ Prob5.java
@@ -0,0 +1,28 @@
+public class Prob5
+{
+    public static void main ()
+    {
+        int flag=0,n=21,i;
+        while (flag!=1)
+        {
+            
+            for(i=1;i<=20;i++)
+            {
+                if(n%i!=0)
+                {
+                    flag=1;
+                    break;
+                }
+                
+            }
+            if(flag==0)
+            {
+                System.out.println("Smallest multiple ="+n);
+                break;
+            }
+            n++;
+            flag=0;
+        }
+    }
+}
+    
